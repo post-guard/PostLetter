e76de624bea0f587e69a1f41d40ca71c3a08e263
Ichirinko <1621543655@qq.com>
2023-06-26 14:58:24 +0800

    fix:更改了message页面列表的逻辑

--- src/main/java/top/rrricardo/postletter/controllers/MessageController.java
+++ src/main/java/top/rrricardo/postletter/controllers/MessageController.java
@@ -1,21 +1,25 @@
 package top.rrricardo.postletter.controllers;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.cell.TextFieldListCell;
+import javafx.util.Callback;
+import javafx.util.StringConverter;
 import top.rrricardo.postletter.exceptions.NetworkException;
-import top.rrricardo.postletter.models.GroupDTO;
 import top.rrricardo.postletter.models.ResponseDTO;
 import top.rrricardo.postletter.models.Session;
-import top.rrricardo.postletter.models.User;
 import top.rrricardo.postletter.services.HttpService;
 import top.rrricardo.postletter.utils.ControllerBase;
 
 import java.io.IOException;
+import java.util.List;
 
 public class MessageController extends HomeController implements ControllerBase {
 
@@ -23,26 +27,51 @@ public class MessageController extends HomeController implements ControllerBase 
     @FXML
     private Button sendButton;
     @FXML
-    private ListView<String> sessionListView = new ListView<>();
+    private Label sessionLabelName;
+    @FXML
+    private Label sessionLabelLevel;
+    @FXML
+    private ListView<Session> sessionListView = new ListView<>();
+
+    private final Callback<ListView<Session>, ListCell<Session>> call = TextFieldListCell.forListView(new StringConverter<>() {
+
+        @Override
+        public String toString(Session object) {
+            return object.getName();
+        }
+
+        @Override
+        public Session fromString(String string) {
+
+            return new Session(string, "", 100);
+        }
+    });
 
 
-    public void open(){
+    public void open() {
         try {
-            var response = HttpService.getInstance().get("/user/",  new TypeReference<ResponseDTO<User[]>>(){});
+            var response = HttpService.getInstance().get("/session/", new TypeReference<ResponseDTO<List<Session>>>() {
+            });
+            System.out.println(response);
+            if (response != null) {
 
-            if(response != null) {
+                List<Session> sessions = response.getData();
 
-                User[] users = response.getData();
+                ObservableList<Session> items = FXCollections.observableArrayList();
 
-                ObservableList<String> items = FXCollections.observableArrayList("今天", "明天");
-                for (int i = 0; i < users.length; i++) {
-                    items.add(users[i].getNickname());
-                    System.out.println(users[i].getNickname());
-                }
+                items.addAll(sessions);
 
                 sessionListView.setItems(items);
+                sessionListView.setCellFactory(call);
+                sessionListView.getSelectionModel().selectedItemProperty().addListener(
+                        (ObservableValue<? extends Session> ov, Session old_val,
+                         Session new_val) -> {
+
+                            sessionLabelName.setText(new_val.getName());
+                            sessionLabelLevel.setText("Lv." + new_val.getLevel());
+                        });
             }
-        } catch(NetworkException | IOException e) {
+        } catch (NetworkException | IOException e) {
             System.out.println("获取会话列表失败");
         }
     }
