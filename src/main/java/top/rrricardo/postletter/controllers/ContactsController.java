06e50c35909629f596003b4d77352bfd1346d965
Lan_G <2911328695@qq.com>
2023-06-26 20:46:03 +0800

    fix:保存一下当前ContactsController

--- src/main/java/top/rrricardo/postletter/controllers/ContactsController.java
+++ src/main/java/top/rrricardo/postletter/controllers/ContactsController.java
@@ -5,18 +5,12 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-
-import javafx.scene.control.cell.ComboBoxListCell;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.HBox;
 
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.text.Font;
 import javafx.util.Callback;
 import top.rrricardo.postletter.exceptions.NetworkException;
 
@@ -55,6 +49,127 @@ public class ContactsController extends HomeController implements ControllerBase
     Button button1;
     @FXML
     Button button2;
+
+    /**
+     * 进入联系人列表，自动加载出好友列表
+     * 同时开启监听器
+     */
+    @Override
+    public void open() {
+        //设置状态变量为1
+        state = 1;
+        //清除原有列表
+        listView.getItems().clear();
+        //限制按钮点击
+        friendsButton.setDisable(true);
+        groupButton.setDisable(false);
+        usersButton.setDisable(false);
+        //清除右半区
+        emptyRightArea();
+
+        try {
+            var response = HttpService.getInstance().get("/friend/" + "?userId=" + Configuration.getInstance().getId(), new TypeReference<ResponseDTO<Friend[]>>() {
+            });
+            if(response != null){
+                Friend[] friends = response.getData();
+                ObservableList<OrientationBase> items = FXCollections.observableArrayList();
+                items.addAll(friends);
+
+                listView.setItems(items);
+                listView.setCellFactory(new Callback<>() {
+                    @Override
+                    public ListCell<OrientationBase> call(ListView<OrientationBase> friendListView) {
+                        return new FriendCell();
+                    }
+                });
+                //开启监听器，监听用户点击ListView上的Item,点击一下就展示这个Item的详细资料
+                listView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<OrientationBase>() {
+                    @Override
+                    public void changed(ObservableValue<? extends OrientationBase> observableValue, OrientationBase oldItem, OrientationBase newItem) {
+                        if(newItem == null){
+                            return;
+                        }
+
+                        //被点击的必定是Friend列表里的Item
+                        if(newItem.getNickname() == null && newItem.getFriendId() != 0){
+                            try {
+                                var response = HttpService.getInstance().get("/user/" + newItem.getFriendId(), new TypeReference<ResponseDTO<User>>(){});
+
+                                if(response != null){
+                                    //获取到好友的User信息
+                                    User tempUser = response.getData();
+                                    circle.setVisible(true);
+                                    circleLabel.setText(String.valueOf(tempUser.getNickname().charAt(0))); //头像只显示第一个字符
+                                    circleLabel.setStyle("-fx-text-fill: white");
+                                    label1a.setText("昵称");
+                                    label1b.setText(tempUser.getNickname());
+                                    label2a.setText("账号");
+                                    label2b.setText(tempUser.getUsername());
+
+                                    button1.setVisible(false);
+                                    button1.setDisable(true);
+                                    button2.setVisible(true);
+                                    button2.setDisable(false);
+                                    button2.setText("删除好友");    //只有删除好友按钮
+                                }
+                            }catch (NetworkException | IOException e){
+                                return;
+                            }
+                        }
+                        //被点击的必定是Group列表里的Item
+                        else if(newItem.getNickname() == null && newItem.getFriendId() == 0){
+                            //待完善
+                            System.out.println("这是group!");
+                        }
+                        //被点击的必定是User列表里的Item
+                        else {
+                            circle.setVisible(true);
+                            circleLabel.setText(String.valueOf(newItem.getNickname().charAt(0))); //头像只显示第一个字符
+                            circleLabel.setStyle("-fx-text-fill: white");
+                            label1a.setText("昵称");
+                            label1b.setText(newItem.getNickname());
+                            label2a.setText("账号");
+                            label2b.setText(newItem.getUsername());
+                            //判断当前查看的用户是不是好友
+                            try {
+                                var response = HttpService.getInstance().get("/friend/" + "?userId=" + Configuration.getInstance().getId(), new TypeReference<ResponseDTO<Friend[]>>() {
+                                });
+                                if(response != null){
+                                    Friend [] friends = response.getData();
+                                    //遍历好友列表
+                                    int i;
+                                    for(i = 0; i < friends.length; i++){
+                                        //是好友，只提供删除好友按钮
+                                        if(friends[i].getFriendId() == newItem.getId()){
+                                            button1.setVisible(false);
+                                            button1.setDisable(true);
+                                            button2.setVisible(true);
+                                            button2.setDisable(false);
+                                            button2.setText("删除好友");
+                                            break;
+                                        }
+                                    }
+                                    //不是好友，只提供添加好友按钮
+                                    if(i == friends.length){
+                                        button1.setVisible(true);
+                                        button1.setDisable(false);
+                                        button1.setText("添加好友");
+                                        button2.setVisible(false);
+                                        button2.setDisable(true);
+                                    }
+                                }
+                            }catch (NetworkException | IOException e){
+                                return;
+                            }
+                        }
+                    }
+                });
+
+            }
+        }catch (NetworkException | IOException e){
+            System.out.println("加载失败");
+        }
+    }
 
     /**
      * 辅助方法，用于清除右半区
@@ -129,36 +244,6 @@ public class ContactsController extends HomeController implements ControllerBase
                     }
                 });
 
-                listView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<OrientationBase>() {
-                    @Override
-                    public void changed(ObservableValue<? extends OrientationBase> observableValue, OrientationBase oldFriend, OrientationBase newFriend) {
-                        if(newFriend != null){
-                            try {
-                                System.out.println("666666id:" + newFriend.getFriendId());
-                                var response = HttpService.getInstance().get("/user/" + newFriend.getFriendId(), new TypeReference<ResponseDTO<User>>(){});
-
-                                if(response != null){
-                                    User newUser = response.getData();
-                                    circle.setVisible(true);
-                                    circleLabel.setText(String.valueOf(newUser.getNickname().charAt(0))); //头像只显示第一个字符
-                                    circleLabel.setStyle("-fx-text-fill: white");
-                                    label1a.setText("昵称");
-                                    label1b.setText(newUser.getNickname());
-                                    label2a.setText("账号");
-                                    label2b.setText(newUser.getUsername());
-
-                                    button1.setVisible(false);
-                                    button1.setDisable(true);
-                                    button2.setVisible(true);
-                                    button2.setDisable(false);
-                                    button2.setText("删除好友");    //只有删除好友按钮
-                                }
-                            }catch (NetworkException | IOException e){
-                                return;
-                            }
-                        }
-                    }
-                });
             }
         }catch (NetworkException | IOException e){
             return;
@@ -250,52 +335,7 @@ public class ContactsController extends HomeController implements ControllerBase
                     }
                 });
 
-                //点击一个用户，这个用户的资料就加载到右半区显示
-                listView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<OrientationBase>() {
-                    @Override
-                    public void changed(ObservableValue<? extends OrientationBase> observableValue, OrientationBase oldUser, OrientationBase newUser) {
-                        if(newUser != null){
-                            circle.setVisible(true);
-                            circleLabel.setText(String.valueOf(newUser.getNickname().charAt(0))); //头像只显示第一个字符
-                            circleLabel.setStyle("-fx-text-fill: white");
-                            label1a.setText("昵称");
-                            label1b.setText(newUser.getNickname());
-                            label2a.setText("账号");
-                            label2b.setText(newUser.getUsername());
-                            //判断当前查看的用户是不是好友
-                            try {
-                                var response = HttpService.getInstance().get("/friend/" + "?userId=" + Configuration.getInstance().getId(), new TypeReference<ResponseDTO<Friend[]>>() {
-                                });
-                                if(response != null){
-                                    Friend [] friends = response.getData();
-                                    //遍历好友列表
-                                    int i;
-                                    for(i = 0; i < friends.length; i++){
-                                        //是好友，只提供删除好友按钮
-                                        if(friends[i].getFriendId() == newUser.getId()){
-                                            button1.setVisible(false);
-                                            button1.setDisable(true);
-                                            button2.setVisible(true);
-                                            button2.setDisable(false);
-                                            button2.setText("删除好友");
-                                            break;
-                                        }
-                                    }
-                                    //不是好友，只提供添加好友按钮
-                                    if(i == friends.length){
-                                        button1.setVisible(true);
-                                        button1.setDisable(false);
-                                        button1.setText("添加好友");
-                                        button2.setVisible(false);
-                                        button2.setDisable(true);
-                                    }
-                                }
-                            }catch (NetworkException | IOException e){
-                                return;
-                            }
-                        }
-                    }
-                });
+
             }
         } catch (NetworkException e) {
             return;
